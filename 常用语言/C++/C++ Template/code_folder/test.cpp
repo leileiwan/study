00b738e612d4e15a7b4c5e8c6a8c38da9f97210a
xiaoluwwwxiaolu <xiaoluwwwxiaolu@gmail.com>
2019-08-22 14:10:08 +0800

    --- Auto-Git Commit ---

--- 常用语言/C++/C++ Template/code_folder/test.cpp
+++ 常用语言/C++/C++ Template/code_folder/test.cpp
@@ -1,15 +1,25 @@
 #include "iostream"
 using namespace std;
 
-template <typename T>
-
-T const& t_max(T const& a,T const& b){
+template <typename T1>
+T1 max(T1 a,T1 b){
     return a>b?a:b;
+}
+
+int max(int a,int b){
+    cout<<"no template..."<<endl;
+    return a>b?a:b;
+}
+
+template <typename T1>
+T1 max(T1 a,T1 b,T1 c){
+    return a>max(b,c):a:max(b,c);
 }
 
 int main(){
     int a=0;
-    float b=1.2;
-    cout << t_max<float>(a,b)<<"\n";
+    int b=1;
+    cout <<max(a,b)<<endl;
+    cout<<max(1.0,2.0,3.0);
     return 0;
 }

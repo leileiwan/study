6320e573eb2c562f474639889783b21f66095b6e
xiaoluwwwxiaolu <xiaoluwwwxiaolu@gmail.com>
2019-08-22 11:09:30 +0800

    --- Auto-Git Commit ---

--- 常用语言/C++/code_folder/test.cpp
+++ 常用语言/C++/code_folder/test.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main(){
+    int a=0;
     cout << "zz";
+    a=1;
     return 0;
 }
+//Unable to open 'libc-start.c': Unable to read file (Error: File not found (/build/glibc-LK5gWL/glibc-2.23/csu/libc-start.c)).

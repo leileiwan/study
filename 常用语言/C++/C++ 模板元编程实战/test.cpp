679fd7376e9643abc7426ce9e99648f5110dc11f
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-12 10:02:34 +0800

    --- Auto-Git Commit ---

--- 常用语言/C++/C++ 模板元编程实战/test.cpp
+++ 常用语言/C++/C++ 模板元编程实战/test.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 
-template <template <typename> typename T1,typename T2>
+template <template <typename> class T1,typename T2>
 struct Fun_
 {
     using type=typename T1<T2>::type;
     /* data */
 };
 
-template <template <typename> typename T1,typename T2>
+template <template <typename> class T1,typename T2>
 using Fun=typename Fun_<T1,T2>::type;
 
 Fun<std::remove_reference,int&> h=3;

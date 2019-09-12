47845c5bbfea51887750e6936eee925b39246fde
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-12 16:33:57 +0800

    --- Auto-Git Commit ---

--- 常用语言/C++/C++ 模板元编程实战/test.cpp
+++ 常用语言/C++/C++ 模板元编程实战/test.cpp
@@ -1,7 +1,19 @@
 #include "iostream"
 using namespace std;
 
+template <typename T>
+struct RemoveReferenceConst_
+{
+    private:
+        using inter_type=typename std::remove_reference<T>::type;
+    public:
+        using type=typename std::remove_const<inter_type>::type;
+};
 
+template<typename T>
+using RemoveReferenceConst=typename RemoveReferenceConst_<T>::type;
+
+RemoveReferenceConst<const int&> h=3;
 
 
 int main(){

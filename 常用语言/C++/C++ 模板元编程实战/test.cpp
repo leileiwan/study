4291184c896c851c5277ed72285829df6d17b040
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-12 16:03:51 +0800

    --- Auto-Git Commit ---

--- 常用语言/C++/C++ 模板元编程实战/test.cpp
+++ 常用语言/C++/C++ 模板元编程实战/test.cpp
@@ -2,55 +2,6 @@
 using namespace std;
 
 
-template <bool AddorRemove> class Fun_;
-
-template<>
-struct Fun_<true>{
-    template<typename T>
-    using type=std::add_lvalue_reference<T>;
-};
-
-template<>
-struct Fun_<false>{
-    template<typename T>
-    using type=std::remove_reference<T>;
-};
-
-
-template<typename T>
-template<bool AddorRemove>
-using Fun=typename Fun_<AddorRemove>::template type<T>;
-
-template<typename T>
-using Res_=Fun<false>;
-
-Res_<int&>::type h=3;
-
-
-
-// template <bool AddorRemove> struct Fun_;
-
-// template <>
-// struct Fun_<true>{
-//     template<typename T>
-//     using type=std::add_lvalue_reference<T>;
-// };
-
-// template <>
-// struct Fun_<false>{
-//     template<typename T>
-//     using type=std::remove_reference<T>;
-// };
-
-// template <typename T>
-//     template <bool AddorRemove>
-//     using Fun= typename Fun_<AddorRemove>::template type<T>;
-
-// template <typename T>
-// using Res_=Fun<false>;
-
-// Res_<int&>::type h=3;
-
 
 
 int main(){

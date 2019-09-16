41dcd05000b710fd04a8b5ac19e864f1b7133a49
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-16 17:00:28 +0800

    --- Auto-Git Commit ---

--- 编程语言/C++/C++ 模板元编程实战/test.cpp
+++ 编程语言/C++/C++ 模板元编程实战/test.cpp
@@ -1,31 +1,31 @@
 #include "iostream"
 using namespace std;
 
+template <size_t ID,typename TDummy=void>
+struct imp{
+    constexpr static size_t value = ID + imp<ID-1>::value;
+};
+
+template<typename TDummy>
+struct imp<0,TDummy>{
+    constexpr static size_t value=0;
+};
+
 template <size_t A>
-class Wrap_{
-    template <size_t ID,typename TDummy=void>
-    class imp{
-        constexpr static size_t value = ID + imp<ID-1>::value;
-    }
-
-    template<typename TDummy>
-    class tmp<0,TDummy>{
-        constexpr static size_t value=0;
-    }
-
+struct Wrap_{
     template <size_t ID>
     constexpr static size_t value=imp<A+ID>::value;
 };
 
-size_t h=Wrap_<0>::value<2>;
-// size_t h1=Wrap_<10>::value<2>;
+size_t h=Wrap_<3>::value<2>;
+size_t h1=Wrap_<10>::value<2>;
 
 
 int main(){
 
     cout<<"hello"<<endl;
     cout<<h<<endl;
-    // cout<<h1<<endl;
+    cout<<h1<<endl;
     return 0;
 }
 

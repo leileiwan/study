03717b70c9e5d4e8f78923c9faf0db6cdd557e94
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-19 10:56:52 +0800

    --- Auto-Git Commit ---

--- 编程语言/C++/C++ 模板元编程实战/test.cpp
+++ 编程语言/C++/C++ 模板元编程实战/test.cpp
@@ -1,31 +1,12 @@
-#include "iostream"
-using namespace std;
-
-template <size_t ID,typename TDummy=void>
-struct imp{
-    constexpr static size_t value = ID + imp<ID-1>::value;
-};
-
-template<typename TDummy>
-struct imp<0,TDummy>{
-    constexpr static size_t value=0;
-};
-
-template <size_t A>
-struct Wrap_{
-    template <size_t ID>
-    constexpr static size_t value=imp<A+ID>::value;
-};
-
-size_t h=Wrap_<3>::value<2>;
-size_t h1=Wrap_<10>::value<2>;
-
-
-int main(){
-
-    cout<<"hello"<<endl;
-    cout<<h<<endl;
-    cout<<h1<<endl;
-    return 0;
-}
-
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    int main(){
+        char *s = "0123456789012345678901234567890";
+        char *p;
+        p = strchr(s, '5');
+        printf("%ld\n", s);
+        printf("%ld\n", p);
+        system("pause");
+        return 0;
+    }

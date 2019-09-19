78793653476a04ec954752034be9b47148985b82
xiaoluwwwxiaolu <873896870@qq.com>
2019-09-19 11:26:57 +0800

    --- Auto-Git Commit ---

--- 编程语言/C++/C++ 模板元编程实战/test.cpp
+++ 编程语言/C++/C++ 模板元编程实战/test.cpp
@@ -1,12 +1,38 @@
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
-    int main(){
-        char *s = "0123456789012345678901234567890";
-        char *p;
-        p = strchr(s, '5');
-        printf("%ld\n", s);
-        printf("%ld\n", p);
-        system("pause");
-        return 0;
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int search(char*s,char*t)
+{	
+	int size_t=strlen(t),size_s=strlen(s),T,i,j;
+	char *p=NULL;
+	for(i=0;i<=(size_s-size_t);i++)
+	{
+		p=s+i;
+		T=1;
+		for(j=0;j<size_t;j++)
+		{
+			if(*p!=*(t+j))
+			{
+				T=0;
+				break;
+			}
+			p++;
+		}
+		if(T==1)
+			break;
+	}
+	if(T==0)
+		return 0;
+	else
+		return 1;
+}
+
+int main(){
+    char *s = "./drive_c/users/dev/Application Data/Tencent/QQ/AuTemp/2653170047/NewUpd/txupd.exe";
+    char *p="tdxupd.exe";
+    int res=search(s,p);
+    printf("%d\n",res);
+    // system("pause");
+    return 0;
+}
